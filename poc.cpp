e0c0f338121e1659e2902ca3004235c07afdd205
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-17 23:37:31 +0000

    parsing numbers and "print"

---
+++ poc.cpp
@@ -0,0 +1,69 @@
+#pragma leco tool
+
+import hai;
+import jojo;
+import jute;
+import silog;
+
+namespace token {
+  enum type {
+    nil,
+    number,
+  };
+  struct t {
+    type type;
+    jute::view content;
+  };
+  using list = hai::chain<t>;
+
+  static t make(type tp, const char * start, const char * end) {
+    unsigned sz = end - start;
+    return { .type = tp, .content = { start, sz } };
+  }
+}
+
+static bool match(const char *& ptr, jute::view token) {
+  for (auto i = 0; i < token.size(); i++) {
+    if ((ptr[i] & ~0x20) != token[i]) return false;
+  }
+  auto e = ptr[token.size()] & ~0x20;
+  if (e >= 'A' && e <= 'Z') return false;
+
+  ptr += token.size();
+  return true;
+}
+
+static token::list tokenise(hai::cstr & src) {
+  token::list res {};
+  const char * ptr = src.begin();
+  while (auto c = *ptr) {
+    auto cs = ptr;
+    if (c == ' ') {
+      ptr++;
+      continue;
+    }
+    if (c >= '0' && c <= '9') {
+      while (*ptr >= '0' && *ptr <= '9') ptr++;
+      res.push_back(token::make(token::number, cs, ptr));
+      continue;
+    }
+    if (match(ptr, "PRINT")) {
+      continue;
+    }
+    silog::die("invalid char: [%c]", *ptr);
+  }
+  return res;
+}
+
+static void compile(void *, hai::cstr & src) {
+  auto ts = tokenise(src);
+  for (auto & t : ts) {
+    silog::log(silog::debug, "%d [%.*s]", t.type, (int)t.content.size(), t.content.begin());
+  }
+}
+
+int main(int argc, char ** argv) try {
+  jojo::read("example1.bas", nullptr, compile);
+} catch (...) {
+  return 1;
+}
