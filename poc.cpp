9fa346012d06a2b2ef6094798a008ef33ac106a5
Eduardo Costa <m4c0@users.noreply.github.com>
2024-11-30 22:25:23 +0000

    mapping line numbers

--- poc.cpp
+++ poc.cpp
@@ -2,12 +2,23 @@
 
 import jojo;
 import jute;
+import hashley;
 import parser;
 import silog;
 
 static void compile(jute::view fname) {
   auto src = jojo::read_cstr(fname);
   auto lines = parse(fname, src);
+
+  unsigned min = ~0;
+  hashley::siobhan l_idx { 113 };
+  for (auto i = 0; i < lines.size(); i++) {
+    auto l_num = lines.seek(i).number;
+    l_idx[l_num] = i;
+    if (l_num < min) min = l_num;
+  }
+  silog::trace("start at", min);
+  silog::trace("line no", l_idx[min]);
   silog::trace("parsed lines", lines.size());
 }
 
