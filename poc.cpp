b036b722a83c51652e09a88166e0293822d8bd68
Eduardo Costa <m4c0@users.noreply.github.com>
2024-12-13 15:51:02 +0000

    replace silog with print
    better for tooling

--- poc.cpp
+++ poc.cpp
@@ -5,8 +5,8 @@ import jute;
 import hai;
 import hashley;
 import parser;
+import print;
 import rng;
-import silog;
 
 enum class var_type {
   nil,
@@ -28,11 +28,11 @@ static hashley::niamh g_var_idx { 113 };
 
 static constexpr void assert_int(const var & v, const char * msg) {
   if (v.type == var_type::integer) return;
-  silog::die("'%s' does not support var type %d", msg, v.type);
+  dief("'%s' does not support var type %d", msg, v.type);
 }
 static constexpr void assert_real(const var & v, const char * msg) {
   if (v.type == var_type::real || v.type == var_type::integer) return;
-  silog::die("'%s' does not support var type %d", msg, v.type);
+  dief("'%s' does not support var type %d", msg, v.type);
 }
 
 static var var_int(int n) {
@@ -63,7 +63,7 @@ static var eval_binop(const ast::node & n) {
     }
     return var_real(lhs.real * rhs.real);
   } 
-  silog::die("cannot eval binary operation '%.*s'",
+  dief("cannot eval binary operation '%.*s'",
       static_cast<unsigned>(n.content.size()),
       n.content.data());
 }
@@ -72,7 +72,7 @@ static var eval_int_cast(const ast::node & n) {
   switch (res.type) {
     case var_type::integer: return res;
     case var_type::real:    return var_int(static_cast<int>(res.real));
-    default: silog::die("cannot cast to int node type: %d", res.type);
+    default: dief("cannot cast to int node type: %d", res.type);
   }
 }
 static var eval_integer(const ast::node & n) { return var_int(n.number); }
@@ -80,12 +80,12 @@ static var eval_rnd(const ast::node & n) {
   auto max = eval((*n.children)[0]);
   switch (max.type) {
     case var_type::integer: return var_real(rng::randf() * max.integer);
-    default: silog::die("invalid random max type: %d", max.type);
+    default: dief("invalid random max type: %d", max.type);
   }
 }
 static var eval_var(const ast::node & n) {
   auto idx = g_var_idx[n.content];
-  if (idx == 0) silog::die("undefined variable [%.*s]",
+  if (idx == 0) dief("undefined variable [%.*s]",
       static_cast<unsigned>(n.content.size()),
       n.content.data());
   return g_vars.seek(idx - 1);
@@ -97,7 +97,7 @@ static var eval(const ast::node & n) {
     case ast::type::integer:  return eval_integer(n);
     case ast::type::rnd:      return eval_rnd(n);
     case ast::type::variable: return eval_var(n);
-    default: silog::die("cannot eval node type: %d", n.type);
+    default: dief("cannot eval node type: %d", n.type);
   }
 }
 
@@ -118,7 +118,7 @@ static void color(const ast::node & n) {
   auto brd = eval((*n.children)[2]);
   assert_int(brd, "color");
 
-  silog::log(silog::debug, "TODO: COLOR %d, %d, %d", fg.integer, bg.integer, brd.integer);
+  putfn("TODO: COLOR %d, %d, %d", fg.integer, bg.integer, brd.integer);
 }
 
 static void go_to(int n) {
@@ -133,11 +133,9 @@ static void go_to(int n) {
 static void print(const ast::node & n) {
   switch (n.type) {
     case ast::type::string:
-      silog::log(silog::info, "%.*s",
-         static_cast<unsigned>(n.content.size()),
-         n.content.data());
+      putln(n.content);
       break;
-    default: silog::die("cannot print node type: %d", n.type);
+    default: dief("cannot print node type: %d", n.type);
   }
 }
 
@@ -149,11 +147,11 @@ static void pset(const ast::node & n) {
   auto c = eval((*n.children)[2]);
   assert_int(c, "pset");
 
-  silog::log(silog::debug, "TODO: PSET(%d, %d),%d", x.integer, y.integer, c.integer);
+  putfn("TODO: PSET(%d, %d),%d", x.integer, y.integer, c.integer);
 }
 
 static void screen(int n) {
-  silog::log(silog::debug, "TODO: SCREEN %d", n);
+  putfn("TODO: SCREEN %d", n);
 }
 
 static void run() {
@@ -165,7 +163,7 @@ static void run() {
     case ast::type::print:  print((*line.children)[0]); break;
     case ast::type::pset:   pset(line); break;
     case ast::type::screen: screen(line.number); break;
-    default: silog::die("invalid node type: %d", line.type);
+    default: dief("invalid node type: %d", line.type);
   }
   g_cur_line++;
 }
